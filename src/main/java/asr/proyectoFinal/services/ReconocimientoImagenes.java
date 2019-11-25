02c9656730f96923420dbfae84b166aac28d1040
Jaime <jaimefusterf@gmail.com>
2019-11-25 23:01:30 +0100

    Añadidos ReconocimientoImagenes.java y TextoAVoz.java

---
+++ src/main/java/asr/proyectoFinal/services/ReconocimientoImagenes.java
@@ -0,0 +1,59 @@
+package asr.proyectoFinal.services;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+
+public class ReconocimientoImagenes{
+	
+	public static String reconocer(String image){
+		IamOptions options = new IamOptions.Builder().apiKey("qhTU6Ut5OkySgZd6qj9TtjxA6VbTmTO4dW_V7y6rgJQZ").build();
+	
+		VisualRecognition visualRecognition = new VisualRecognition("2019-04-11", options);
+	
+		ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+				.url(image)
+				.build();
+		ClassifiedImages result = visualRecognition.classify(classifyOptions).execute();
+		System.out.println(result);
+		
+
+		String imagenJSON = result.toString();
+		JsonParser parser = new JsonParser();
+		JsonObject rootObj = parser.parse(imagenJSON).getAsJsonObject(); //Cogemos todo el JSON
+		JsonArray imagenes = rootObj.getAsJsonArray("images");
+		
+		JsonObject obj2 = (JsonObject)imagenes.get(0);
+		JsonArray clasificaciones = obj2.getAsJsonArray("classifiers");
+		
+		JsonObject obj3 = (JsonObject)clasificaciones.get(0);
+		JsonArray clases = obj3.getAsJsonArray("classes");
+		
+		double score=0;
+		String clase=image;
+		for(int i=0; i<clases.size();i++)
+		{
+			double scoreResult=clases.get(i).getAsJsonObject().get("score").getAsDouble();
+			if(scoreResult>score)
+			{
+				score=scoreResult;
+				clase=clases.get(i).getAsJsonObject().get("class").getAsString();
+			}
+			
+		}
+			
+		System.out.println(clase);
+		
+		
+//		if(imagenes.size()>0)
+//			imagenPrimera = imagenes.get(0).getAsJsonObject().get("classifiers").getAsString();
+//		
+//		System.out.println(imagenPrimera);
+		return clase;
+	}
+
+}
---
+++ src/main/java/asr/proyectoFinal/services/TextoAVoz.java
@@ -0,0 +1,59 @@
+package asr.proyectoFinal.services;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.*;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.model.SynthesizeOptions;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.util.WaveUtils;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.websocket.BaseSynthesizeCallback;
+
+public class TextoAVoz {
+	public static void hablar(String texto,HttpServletResponse response) {
+		IamOptions options = new IamOptions.Builder().apiKey("viELcj1khvVnaWZC16efrUoMwOGF5txO50TfRmcLU_DH").build();
+
+		TextToSpeech textToSpeech = new TextToSpeech(options);
+		textToSpeech.setEndPoint("https://gateway-lon.watsonplatform.net/text-to-speech/api");
+
+		try {
+			SynthesizeOptions synthesizeOptions = new SynthesizeOptions
+					.Builder()
+					.text(texto)
+					.accept("audio/webm")
+					.voice("es-ES_LauraVoice")
+					.build();
+
+			InputStream inputStream = textToSpeech.synthesize(synthesizeOptions).execute();//.getResult();
+			//InputStream in = WaveUtils.reWriteWaveHeader(inputStream);
+			response.reset();
+			OutputStream out = response.getOutputStream();
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = inputStream.read(buffer)) > 0) {
+				out.write(buffer, 0, length);
+			}
+			out.close();
+			//in.close();
+			inputStream.close();
+						
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return ;
+	}
+}
